360cc9a5cf581cee3789258961bc41cfc6058a15
kike <kikeramos9@gmail.com>
2020-05-24 19:00:46 +0200

    fixed API_Emp_Test

--- src/front/GUI_Enrique/EmpGraphTwo.svelte
+++ src/front/GUI_Enrique/EmpGraphTwo.svelte
@@ -1,1 +1,55 @@
-<h1 style="text-align:center">Gráfica 2 </h1>
+<script>
+
+    async function loadGraph() {
+
+        let MyData = [];
+        let MyDataArray = [];
+        let MyDataEmp = [];
+
+        const resData = await fetch("/api/v2/emp-stats");
+        MyData = await resData.json();
+        
+        MyData.forEach( (e) => {
+            
+            MyDataArray.push({name: e.country + " " + e.year, y: e.emp_vuln_female})
+            MyDataEmp.push({name: 'Empleo vulnerable femenino', data: MyDataArray});
+                       
+        });
+        
+        new Morris.Line({
+        // ID of the element in which to draw the chart.
+        element: 'myfirstchart',
+        // Chart data records -- each entry in this array corresponds to a point on
+        // the chart.
+        data: [
+            { year: '2008', value: 20 },
+            { year: '2009', value: 10 },
+            { year: '2010', value: 5 },
+            { year: '2011', value: 5 },
+            { year: '2012', value: 20 }
+        ],
+        // The name of the data record attribute that contains x-values.
+        xkey: 'year',
+        // A list of names of data record attributes that contain y-values.
+        ykeys: ['value'],
+        // Labels for the ykeys -- will be displayed when you hover over the
+        // chart.
+        labels: ['Value']
+        });
+    }
+    
+
+</script>
+
+<svelte:head> 
+    <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/morris.js/0.5.1/morris.css">
+    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.0/jquery.min.js"></script>
+    <script src="//cdnjs.cloudflare.com/ajax/libs/raphael/2.1.0/raphael-min.js"></script>
+    <script src="//cdnjs.cloudflare.com/ajax/libs/morris.js/0.5.1/morris.min.js" on:load="{loadGraph}"></script>
+</svelte:head>
+
+<main>
+    <h1 style="text-align:center">Gráfica 2 </h1>
+    <div id="myfirstchart" style="height: 250px;"></div>
+
+</main>
