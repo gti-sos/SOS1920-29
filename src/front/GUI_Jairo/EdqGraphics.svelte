1bf556bd42c6ee74b6df6056bd95bab4f20b4a71
jairo25fdez <jairo25fdez@gmail.com>
2020-05-25 17:41:38 +0200

    Added 2nd graphic chart.js to Edq_Stats

--- src/front/GUI_Jairo/EdqGraphics.svelte
+++ src/front/GUI_Jairo/EdqGraphics.svelte
@@ -2,8 +2,10 @@
 
     import Button from "sveltestrap/src/Button.svelte";
     import {pop} from "svelte-spa-router";
+    import Chart from 'chart.js';
 
     const BASE_API_URL = "/api/v1";
+    
 
     async function loadGraphs(){
 
@@ -113,6 +115,8 @@
 
     async function loadGraph2(loadedData){
 
+        //new Chart(document.getElementById("chartjs_graph"),{"type":"radar","data":{"labels":["Eating","Drinking","Sleeping","Designing","Coding","Cycling","Running"],"datasets":[{"label":"My First Dataset","data":[65,59,90,81,56,55,40],"fill":true,"backgroundColor":"rgba(255, 99, 132, 0.2)","borderColor":"rgb(255, 99, 132)","pointBackgroundColor":"rgb(255, 99, 132)","pointBorderColor":"#fff","pointHoverBackgroundColor":"#fff","pointHoverBorderColor":"rgb(255, 99, 132)"},{"label":"My Second Dataset","data":[28,48,40,19,96,27,100],"fill":true,"backgroundColor":"rgba(54, 162, 235, 0.2)","borderColor":"rgb(54, 162, 235)","pointBackgroundColor":"rgb(54, 162, 235)","pointBorderColor":"#fff","pointHoverBackgroundColor":"#fff","pointHoverBorderColor":"rgb(54, 162, 235)"}]},"options":{"elements":{"line":{"tension":0,"borderWidth":3}}}});
+
     }
 
     async function loadData(){
@@ -152,7 +156,10 @@
         <div id="container"></div>
     </figure>
 
-    <h3>Gráfica 2 EDQ_Stats (Chart.js)</h3>
+    <h3>Análisis de la calidad educativa en España.</h3>
+    <figure class="chartjs-figure">
+        <canvas id="chartjs_graph"></canvas>
+    </figure>
 
     <Button outline color = "secondary" on:click="{pop}">Volver</Button>
 
