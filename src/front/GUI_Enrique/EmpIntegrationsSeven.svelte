8b3486406fcde5165a3026a13e09ab2fea50163b
kike <kikeramos9@gmail.com>
2020-05-29 20:09:40 +0200

    added 7 integrations

--- src/front/GUI_Enrique/EmpIntegrationsSeven.svelte
+++ src/front/GUI_Enrique/EmpIntegrationsSeven.svelte
@@ -7,134 +7,92 @@
 
     async function loadGraph(){
 
-        const res = await fetch("https://transit.land/api/v1/schedule_stop_pairs?bbox=-121.0,35.0,-124.0,37.0");
-        let Array_final = [];
-        let Array_origin = [];
-        let Array_dest = [];
+        const res = await fetch("https://api.tfl.gov.uk/Mode/tube/Arrivals?count=1");
+        let Array_data = [];
         let Array_category = [];
 
         if(res.ok){
             
             let json = await res.json();
-            let data_rutas = json;
+            let data_tube = json;
             let cont = 0;
 
-            let data_rutas_array = Object.values(data_rutas)
-            //console.log(JSON.stringify(data_rutas_array,null,2));
-            data_rutas_array.forEach( (e) => {
-                //console.log(JSON.stringify(e,null,2));
-                for(let i = 0; i< e.length; i++){
-
-                    cont += 1;
-                    if(cont <= 3){
-                        
-                        Array_origin.push(e[i].origin_dist_traveled);
-                        Array_dest.push(e[i].destination_dist_traveled);
-                        Array_category.push(e[i].trip_headsign);
-                        
-                    }
+            data_tube.forEach( (e) => {
+                cont += 1;
+                if(cont <= 7){
+                    Array_data.push(e.timeToStation);
+                    Array_category.push(e.stationName);
                 }
-        
-            }); 
-
-            Array_final.push({name: 'distancia de origen', data: Array_origin});
-            Array_final.push({name: 'distancia de destino', data: Array_dest});
+                
+            });
+            
 
         }else{
             console.log("No se ha podido acceder a la API");
         }
 
-        Highcharts.chart('container', {
-            chart: {
-                type: 'bar'
-            },
+        var chart = Highcharts.chart('container', {
+
             title: {
-                text: 'Rutas San Francisco'
+                text: 'Chart.update'
             },
+
             subtitle: {
-                text: ''
+                text: 'Plain'
             },
+
             xAxis: {
-                categories: Array_category,
-                title: {
-                    text: null
-                }
+                categories: Array_category
             },
-            yAxis: {
-                min: 0,
-                title: {
-                    text: 'miles (Kms)',
-                    align: 'high'
-                },
-                labels: {
-                    overflow: 'justify'
-                }
-            },
-            tooltip: {
-                valueSuffix: ' miles'
-            },
-            plotOptions: {
-                bar: {
-                    dataLabels: {
-                        enabled: true
-                    }
-                }
-            },
-            legend: {
-                layout: 'vertical',
-                align: 'right',
-                verticalAlign: 'top',
-                x: -40,
-                y: 140,
-                floating: true,
-                borderWidth: 1,
-                backgroundColor:
-                    Highcharts.defaultOptions.legend.backgroundColor || '#FFFFFF',
-                shadow: true
-            },
-            credits: {
-                enabled: false
-            },
-            series: Array_final
+
+            series: [{
+                type: 'column',
+                colorByPoint: true,
+                data: Array_data,
+                showInLegend: false
+            }]
+
         });
 
-       
     }
     
 </script>
 
 <svelte:head>
+    <script src="https://code.jquery.com/jquery-3.1.1.min.js"></script>
     <script src="https://code.highcharts.com/highcharts.js"></script>
+    <script src="https://code.highcharts.com/highcharts-more.js"></script>
     <script src="https://code.highcharts.com/modules/exporting.js"></script>
     <script src="https://code.highcharts.com/modules/export-data.js"></script>
     <script src="https://code.highcharts.com/modules/accessibility.js" on:load="{loadGraph}"></script>
-</svelte:head>
+    </svelte:head>
 
 <main>
 
-    <h1 style="text-align:center">Integración 4</h1>
-    <h4 style="text-align:center"><a href="https://apis.is/earthquake/is">Transitland Datastore API</a></h4>
+    <h1 style="text-align:center">Integración 7</h1>
+    <h4 style="text-align:center"><a href="https://api.tfl.gov.uk/Mode/tube/Arrivals?count=1">Transitland Datastore API</a></h4>
     <h5 style="text-align:center">Esta API se encarga de ofrecernos rutas operadas por la Agencia de Transporte Municipal de San Francisco (SFMTA).</h5>
 
     <figure class="highcharts-figure">
         <div id="container"></div>
         <p class="highcharts-description">
-            La gráfica nos muestra la distancia de origen y destino recorrida de algunas rutas.
+            Chart with buttons to modify options, showing how options can be changed
+            on the fly. This flexibility allows for more dynamic charts.
         </p>
     </figure>
     <Button outline color="secondary" on:click="{pop}">Volver</Button>
-
+    
 </main>
 
 <style>
-    .highcharts-figure, .highcharts-data-table table {
-    min-width: 310px; 
-    max-width: 800px;
-    margin: 1em auto;
+    #container {
+    height: 400px; 
     }
 
-    #container {
-        height: 400px;
+    .highcharts-figure, .highcharts-data-table table {
+        min-width: 320px; 
+        max-width: 800px;
+        margin: 1em auto;
     }
 
     .highcharts-data-table table {
@@ -164,5 +122,6 @@
     .highcharts-data-table tr:hover {
         background: #f1f7ff;
     }
+
 
 </style>
